126dfe7567cf99f084ddf5e01c76620b44a3b740
dera- <alchemy.and.fairy@gmail.com>
2024-03-27 23:15:48 +0900

    reflect pr comment

--- src/main/convert/convertPlugins.ts
+++ src/main/convert/convertPlugins.ts
@@ -1,148 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as acorn from "acorn";
-import * as escodegen from "escodegen";
 import type { TkoolmvPlugin } from "./tkoolmvPlugin";
-
-const TARGET_OBJECT_NAMES = [
-	"DataManager",
-	"Bitmap",
-	"CacheEntry",
-	"Graphics",
-	"ImageCache",
-	"Input",
-	"JsonEx",
-	"Rectangle",
-	"RequestQueue",
-	"ScreenSprite",
-	"Sprite",
-	"Stage",
-	"Tilemap",
-	"TilingSprite",
-	"ToneFilter",
-	"ToneSprite",
-	"TouchInput",
-	"Utils",
-	"Weather",
-	"Window",
-	"WindowLayer",
-	"ImageManager",
-	"PluginManager",
-	"SceneManager",
-	"SoundManager",
-	"StorageManager",
-	"TextManager",
-	"AudioManager",
-	"BattleManager",
-	"ConfigManager",
-	"Game_Action",
-	"Game_ActionResult",
-	"Game_Actor",
-	"Game_Actors",
-	"Game_Battler",
-	"Game_BattlerBase",
-	"Game_Character",
-	"Game_CharacterBase",
-	"Game_CommonEvent",
-	"Game_Enemy",
-	"Game_Event",
-	"Game_Follower",
-	"Game_Followers",
-	"Game_Interpreter",
-	"Game_Item",
-	"Game_Map",
-	"Game_Message",
-	"Game_Party",
-	"Game_Picture",
-	"Game_Player",
-	"Game_Screen",
-	"Game_SelfSwitches",
-	"Game_Switches",
-	"Game_System",
-	"Game_Temp",
-	"Game_Timer",
-	"Game_Troop",
-	"Game_Unit",
-	"Game_Variables",
-	"Game_Vehicle",
-	"Scene_Base",
-	"Scene_Battle",
-	"Scene_Boot",
-	"Scene_Equip",
-	"Scene_GameEnd",
-	"Scene_Gameover",
-	"Scene_Item",
-	"Scene_ItemBase",
-	"Scene_Map",
-	"Scene_Menu",
-	"Scene_MenuBase",
-	"Scene_Options",
-	"Scene_Shop",
-	"Scene_Skill",
-	"Scene_Status",
-	"Scene_Title",
-	"Scene_File",
-	"Scene_Load",
-	"Scene_Save",
-	"Sprite_Actor",
-	"Sprite_Animation",
-	"Sprite_Balloon",
-	"Sprite_Base",
-	"Sprite_Battler",
-	"Sprite_Character",
-	"Sprite_Damage",
-	"Sprite_Destination",
-	"Sprite_Enemy",
-	"Sprite_Picture",
-	"Sprite_StateIcon",
-	"Sprite_StateOverlay",
-	"Sprite_Timer",
-	"Sprite_Weapon",
-	"Spriteset_Base",
-	"Spriteset_Battle",
-	"Spriteset_Map",
-	"Window_ActorCommand",
-	"Window_Base",
-	"Window_BattleActor",
-	"Window_BattleEnemy",
-	"Window_BattleItem",
-	"Window_BattleLog",
-	"Window_BattleSkill",
-	"Window_ChoiceList",
-	"Window_Command",
-	"Window_EquipCommand",
-	"Window_EquipItem",
-	"Window_EquipSlot",
-	"Window_EquipStatus",
-	"Window_EventItem",
-	"Window_GameEnd",
-	"Window_Gold",
-	"Window_Help",
-	"Window_HorzCommand",
-	"Window_ItemCategory",
-	"Window_ItemList",
-	"Window_MapName",
-	"Window_MenuActor",
-	"Window_MenuCommand",
-	"Window_MenuStatus",
-	"Window_Message",
-	"Window_NumberInput",
-	"Window_Options",
-	"Window_PartyCommand",
-	"Window_ScrollText",
-	"Window_Selectable",
-	"Window_ShopBuy",
-	"Window_ShopCommand",
-	"Window_ShopNumber",
-	"Window_ShopSell",
-	"Window_ShopStatus",
-	"Window_SkillList",
-	"Window_SkillStatus",
-	"Window_SkillType",
-	"Window_Status",
-	"Window_TitleCommand",
-	"Window_SavefileList"
-];
 
 const TARGET_VARIABLE_NAMES = [
 	"$gameVariables",
@@ -196,79 +55,156 @@ export function convertPlugins(targetPlugins: TkoolmvPlugin[], srcDir: string, d
 }
 
 export function modifyPluginsJs(src: string): string {
-	const ast = acorn.parse(src, { ecmaVersion: 6 });
-	let modifiedJsonString = JSON.stringify(ast);
-	TARGET_OBJECT_NAMES.forEach(name => {
-		modifiedJsonString = modifiedJsonString.replace(new RegExp(`"${name}"`, "g"), `"tkool_1.${name}"`);
-	});
+	acorn.parse(src, { ecmaVersion: 6 }); // 構文チェックのために AST にパースする
+	let modifiedSrc = src;
+	// $~ のグローバル変数には後から値が代入されるので毎回直接DataManagerから動的に値を取得する必要がある。そのため置換が必要になる
 	TARGET_VARIABLE_NAMES.forEach(name => {
-		modifiedJsonString = modifiedJsonString.replace(new RegExp(`"\\${name}"`, "g"), `"DataManager_1.${name}"`);
-	});
-	const modifiedAst = JSON.parse(modifiedJsonString);
-	let modifiedSrc = escodegen.generate(modifiedAst);
-	// キットで定義したクラスに直接代入はできないため、Object.assign()に書き換え
-	modifiedSrc = modifiedSrc.replace(new RegExp("(tkool_1.[0-9a-zA-Z_$]+) *= *([0-9a-zA-Z_$.]+);?", "g"), "Object.assign($1, $2);");
+		modifiedSrc = modifiedSrc.replace(new RegExp(`\\${name}(?![0-9a-zA-Z_$])`, "g"), `DataManager_1.${name}`)
+	})
 	return `Object.defineProperty(exports, "__esModule", { value: true });
 var tkool_1 = require("../");
 var DataManager_1 = require("../managers/DataManager");
-Number.prototype.clamp = function(min, max) {
-	return Math.min(Math.max(this, min), max);
-};
 
-Number.prototype.mod = function(n) {
-	return ((this % n) + n) % n;
-};
+var DataManager = tkool_1.DataManager
+var Bitmap = tkool_1.Bitmap;
+var CacheEntry = tkool_1.CacheEntry;
+var Graphics = tkool_1.Graphics;
+var ImageCache = tkool_1.ImageCache;
+var Input = tkool_1.Input;
+var JsonEx = tkool_1.JsonEx;
+var Rectangle = tkool_1.Rectangle;
+var RequestQueue = tkool_1.RequestQueue;
+var ScreenSprite = tkool_1.ScreenSprite;
+var Sprite = tkool_1.Sprite;
+var Stage = tkool_1.Stage;
+var Tilemap = tkool_1.Tilemap;
+var TilingSprite = tkool_1.TilingSprite;
+var ToneFilter = tkool_1.ToneFilter;
+var ToneSprite = tkool_1.ToneSprite;
+var TouchInput = tkool_1.TouchInput;
+var Utils = tkool_1.Utils;
+var Weather = tkool_1.Weather;
+var Window = tkool_1.Window;
+var WindowLayer = tkool_1.WindowLayer;
+var ImageManager = tkool_1.ImageManager;
+var PluginManager = tkool_1.PluginManager;
+var SceneManager = tkool_1.SceneManager;
+var SoundManager = tkool_1.SoundManager;
+var StorageManager = tkool_1.StorageManager;
+var TextManager = tkool_1.TextManager;
+var AudioManager = tkool_1.AudioManager;
+var BattleManager = tkool_1.BattleManager;
+var ConfigManager = tkool_1.ConfigManager;
+var Game_Action = tkool_1.Game_Action;
+var Game_ActionResult = tkool_1.Game_ActionResult;
+var Game_Actor = tkool_1.Game_Actor;
+var Game_Actors = tkool_1.Game_Actors;
+var Game_Battler = tkool_1.Game_Battler;
+var Game_BattlerBase = tkool_1.Game_BattlerBase;
+var Game_Character = tkool_1.Game_Character;
+var Game_CharacterBase = tkool_1.Game_CharacterBase;
+var Game_CommonEvent = tkool_1.Game_CommonEvent;
+var Game_Enemy = tkool_1.Game_Enemy;
+var Game_Event = tkool_1.Game_Event;
+var Game_Follower = tkool_1.Game_Follower;
+var Game_Followers = tkool_1.Game_Followers;
+var Game_Interpreter = tkool_1.Game_Interpreter;
+var Game_Item = tkool_1.Game_Item;
+var Game_Map = tkool_1.Game_Map;
+var Game_Message = tkool_1.Game_Message;
+var Game_Party = tkool_1.Game_Party;
+var Game_Picture = tkool_1.Game_Picture;
+var Game_Player = tkool_1.Game_Player;
+var Game_Screen = tkool_1.Game_Screen;
+var Game_SelfSwitches = tkool_1.Game_SelfSwitches;
+var Game_Switches = tkool_1.Game_Switches;
+var Game_System = tkool_1.Game_System;
+var Game_Temp = tkool_1.Game_Temp;
+var Game_Timer = tkool_1.Game_Timer;
+var Game_Troop = tkool_1.Game_Troop;
+var Game_Unit = tkool_1.Game_Unit;
+var Game_Variables = tkool_1.Game_Variables;
+var Game_Vehicle = tkool_1.Game_Vehicle;
+var Scene_Base = tkool_1.Scene_Base;
+var Scene_Battle = tkool_1.Scene_Battle;
+var Scene_Boot = tkool_1.Scene_Boot;
+var Scene_Equip = tkool_1.Scene_Equip;
+var Scene_GameEnd = tkool_1.Scene_GameEnd;
+var Scene_Gameover = tkool_1.Scene_Gameover;
+var Scene_Item = tkool_1.Scene_Item;
+var Scene_ItemBase = tkool_1.Scene_ItemBase;
+var Scene_Map = tkool_1.Scene_Map;
+var Scene_Menu = tkool_1.Scene_Menu;
+var Scene_MenuBase = tkool_1.Scene_MenuBase;
+var Scene_Options = tkool_1.Scene_Options;
+var Scene_Shop = tkool_1.Scene_Shop;
+var Scene_Skill = tkool_1.Scene_Skill;
+var Scene_Status = tkool_1.Scene_Status;
+var Scene_Title = tkool_1.Scene_Title;
+var Scene_File = tkool_1.Scene_File;
+var Scene_Load = tkool_1.Scene_Load;
+var Scene_Save = tkool_1.Scene_Save;
+var Sprite_Actor = tkool_1.Sprite_Actor;
+var Sprite_Animation = tkool_1.Sprite_Animation;
+var Sprite_Balloon = tkool_1.Sprite_Balloon;
+var Sprite_Base = tkool_1.Sprite_Base;
+var Sprite_Battler = tkool_1.Sprite_Battler;
+var Sprite_Character = tkool_1.Sprite_Character;
+var Sprite_Damage = tkool_1.Sprite_Damage;
+var Sprite_Destination = tkool_1.Sprite_Destination;
+var Sprite_Enemy = tkool_1.Sprite_Enemy;
+var Sprite_Picture = tkool_1.Sprite_Picture;
+var Sprite_StateIcon = tkool_1.Sprite_StateIcon;
+var Sprite_StateOverlay = tkool_1.Sprite_StateOverlay;
+var Sprite_Timer = tkool_1.Sprite_Timer;
+var Sprite_Weapon = tkool_1.Sprite_Weapon;
+var Spriteset_Base = tkool_1.Spriteset_Base;
+var Spriteset_Battle = tkool_1.Spriteset_Battle;
+var Spriteset_Map = tkool_1.Spriteset_Map;
+var Window_ActorCommand = tkool_1.Window_ActorCommand;
+var Window_Base = tkool_1.Window_Base;
+var Window_BattleActor = tkool_1.Window_BattleActor;
+var Window_BattleEnemy = tkool_1.Window_BattleEnemy;
+var Window_BattleItem = tkool_1.Window_BattleItem;
+var Window_BattleLog = tkool_1.Window_BattleLog;
+var Window_BattleSkill = tkool_1.Window_BattleSkill;
+var Window_ChoiceList = tkool_1.Window_ChoiceList;
+var Window_Command = tkool_1.Window_Command;
+var Window_EquipCommand = tkool_1.Window_EquipCommand;
+var Window_EquipItem = tkool_1.Window_EquipItem;
+var Window_EquipSlot = tkool_1.Window_EquipSlot;
+var Window_EquipStatus = tkool_1.Window_EquipStatus;
+var Window_EventItem = tkool_1.Window_EventItem;
+var Window_GameEnd = tkool_1.Window_GameEnd;
+var Window_Gold = tkool_1.Window_Gold;
+var Window_Help = tkool_1.Window_Help;
+var Window_HorzCommand = tkool_1.Window_HorzCommand;
+var Window_ItemCategory = tkool_1.Window_ItemCategory;
+var Window_ItemList = tkool_1.Window_ItemList;
+var Window_MapName = tkool_1.Window_MapName;
+var Window_MenuActor = tkool_1.Window_MenuActor;
+var Window_MenuCommand = tkool_1.Window_MenuCommand;
+var Window_MenuStatus = tkool_1.Window_MenuStatus;
+var Window_Message = tkool_1.Window_Message;
+var Window_NumberInput = tkool_1.Window_NumberInput;
+var Window_Options = tkool_1.Window_Options;
+var Window_PartyCommand = tkool_1.Window_PartyCommand;
+var Window_ScrollText = tkool_1.Window_ScrollText;
+var Window_Selectable = tkool_1.Window_Selectable;
+var Window_ShopBuy = tkool_1.Window_ShopBuy;
+var Window_ShopCommand = tkool_1.Window_ShopCommand;
+var Window_ShopNumber = tkool_1.Window_ShopNumber;
+var Window_ShopSell = tkool_1.Window_ShopSell;
+var Window_ShopStatus = tkool_1.Window_ShopStatus;
+var Window_SkillList = tkool_1.Window_SkillList;
+var Window_SkillStatus = tkool_1.Window_SkillStatus;
+var Window_SkillType = tkool_1.Window_SkillType;
+var Window_Status = tkool_1.Window_Status;
+var Window_TitleCommand = tkool_1.Window_TitleCommand;
+var Window_SavefileList = tkool_1.Window_SavefileList;
 
-String.prototype.format = function() {
-	var args = arguments;
-	return this.replace(/%([0-9]+)/g, function(s, n) {
-		return args[Number(n) - 1];
-	});
-};
+require("../../alterGlobalObjects").alterGlobalObjects();
 
-String.prototype.padZero = function(length){
-	var s = this;
-	while (s.length < length) {
-		s = '0' + s;
-	}
-	return s;
-};
-
-Number.prototype.padZero = function(length){
-	return String(this).padZero(length);
-};
-
-Array.prototype.equals = function() {
-	if (!array || this.length !== array.length) {
-		return false;
-	}
-	for (var i = 0; i < this.length; i++) {
-		if (this[i] instanceof Array && array[i] instanceof Array) {
-			if (!this[i].equals(array[i])) {
-				return false;
-			}
-		} else if (this[i] !== array[i]) {
-			return false;
-		}
-	}
-	return true;
-};
-
-Array.prototype.clone = function() {
-	return this.slice(0);
-};
-
-Array.prototype.contains = function(element) {
-	return this.indexOf(element) >= 0;
-};
-
-String.prototype.contains = function(string) {
-	return this.indexOf(string) >= 0;
-};
-
-Math.randomInt = function(max) {
-	return Math.floor(max * Math.random());
-};
 ${modifiedSrc}
 `;
 }
