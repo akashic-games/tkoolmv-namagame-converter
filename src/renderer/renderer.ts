7923afbae01023ce0a31ed49ffe1c3fd1d14cf31
dera- <michihiro_onodera@dwango.co.jp>
2025-08-26 19:37:43 +0900

    set only allow-scripts to iframe sandbox

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -119,9 +119,9 @@ window.addEventListener("load", () => {
 			iframElement.src = response.gameContentUrl;
 			iframElement.width = "100%";
 			iframElement.height = "100%";
-			// スクリプトの実行と同一オリジンリソースへのアクセス以外を許可しないようにsandbox属性を設定する
+			// スクリプトの実行以外を許可しないようにsandbox属性を設定する
 			// sandboxはreadonlyなのでas anyとする
-			(iframElement as any).sandbox = "allow-scripts allow-same-origin";
+			(iframElement as any).sandbox = "allow-scripts";
 			dropZone.append(iframElement);
 			download.disabled = false;
 			reset.disabled = false;
